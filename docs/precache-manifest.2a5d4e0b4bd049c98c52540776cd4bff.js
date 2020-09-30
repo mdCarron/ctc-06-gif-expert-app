321b985029ea6e6268f56464edd46c229091c6d5
mdCarron <mdcarron.dev@gmail.com>
2020-09-30 11:20:47 -0300

    prepara build para github pages

---
+++ docs/precache-manifest.2a5d4e0b4bd049c98c52540776cd4bff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3c3d8b4f74f2479c5c76fa98b13a21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca5279038401e449bc64",
+    "url": "/static/css/main.06f2bfc5.chunk.css"
+  },
+  {
+    "revision": "5771ca48ef21fb3afa3c",
+    "url": "/static/js/2.01467929.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.01467929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5279038401e449bc64",
+    "url": "/static/js/main.93cbd3e2.chunk.js"
+  },
+  {
+    "revision": "0697f89c47ce0e014697",
+    "url": "/static/js/runtime-main.24ed1ae0.js"
+  }
+]);
