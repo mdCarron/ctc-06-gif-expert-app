db4c49cadda460b0539f832d29c11ae64efe50bf
mdCarron <mdcarron.dev@gmail.com>
2020-10-10 14:29:26 -0300

    Completa 4. Implementando PropTypes y Actualizar Snapshots

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -4,8 +4,11 @@ import { shallow } from "enzyme";
 import GifGridItem from "../../components/GifGridItem";
 
 describe("Test del componente <GifGridItem />", () => {
+  const title = "Titulo de string";
+  const url = "https://localhost/direccion-en-url.jpg";
+
   test("should renderizar el componente correctamente", () => {
-    const wrapper = shallow(<GifGridItem />);
+    const wrapper = shallow(<GifGridItem title={title} url={url} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
