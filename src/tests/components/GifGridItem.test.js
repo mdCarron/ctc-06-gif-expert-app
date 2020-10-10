963d6fd0fd845f341e08925d4626f90ec2646e2f
mdCarron <mdcarron.dev@gmail.com>
2020-10-10 14:45:39 -0300

    Completa 5. Pruebas del componente - GifGridItem

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -6,9 +6,25 @@ import GifGridItem from "../../components/GifGridItem";
 describe("Test del componente <GifGridItem />", () => {
   const title = "Titulo de string";
   const url = "https://localhost/direccion-en-url.jpg";
+  const wrapper = shallow(<GifGridItem title={title} url={url} />);
 
   test("should renderizar el componente correctamente", () => {
-    const wrapper = shallow(<GifGridItem title={title} url={url} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test("should prop titulo mostrarse dentro de un tag p", () => {
+    const p = wrapper.find("p");
+    expect(p.text().trim()).toBe(title);
+  });
+
+  test("should props de tag img recibir los props", () => {
+    const img = wrapper.find("img");
+    expect(img.prop("src")).toBe(url);
+    expect(img.prop("alt")).toBe(title);
+  });
+
+  test("should div contener la class animate__fadeInLeft", () => {
+    const div = wrapper.find("div");
+    expect(div.prop("className")).toContain("animate__fadeInLeft");
   });
 });
