8f0b09f6455ec8691bdffec9df8fd6da85bd668f
mdCarron <mdcarron.dev@gmail.com>
2020-10-11 14:40:40 -0300

    Completa 15. customHook - waitForNextUpdate

--- src/tests/hooks/useFetchGif.test.js
+++ src/tests/hooks/useFetchGif.test.js
@@ -2,12 +2,28 @@ import { useFetchGif } from "../../hooks/useFetchGif";
 import { renderHook } from "@testing-library/react-hooks";
 
 describe("testing de custom hook useFetchGif", () => {
-  test("should retornar state inicial", () => {
-    const { result } = renderHook(() => useFetchGif("super nintendo"));
+  test("should retornar state inicial", async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGif("super nintendo")
+    );
 
     const { data, loading } = result.current;
 
+    await waitForNextUpdate();
+
     expect(data).toEqual([]);
     expect(loading).toBe(true);
+  });
+
+  test("should retornar array de 10 gifs y loading en false", async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGif("super nintendo")
+    );
+    await waitForNextUpdate();
+
+    const { data, loading } = result.current;
+
+    expect(data.length).toBe(10);
+    expect(loading).toBe(false);
   });
 });
